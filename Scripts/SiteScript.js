aa94f3733aef85fa1866b1f428e43f2801c29f0e
AMoore1025 <creeperkillr05@gmail.com>
2026-07-29 00:06:25 -0400

    Updated scrolling animations, tweaked content page, adjusted settings slider

--- Scripts/SiteScript.js
+++ Scripts/SiteScript.js
@@ -1,15 +1,17 @@
 // Code for main body content
+
 const observer = new IntersectionObserver((entries) => {
 
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        }
-
-        if (!entry.isIntersecting) {
+        } else {
             entry.target.classList.remove("show");
         }
+        console.log(entry.intersectionRatio);
     });
+}, {
+    rootMargin: "-400px 0px -300px 0px"
 });
 
 const projects = document.querySelectorAll(".project");
